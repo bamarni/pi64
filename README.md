831fbfcfcc37da8b5a7629d6b4d1d91a65670210
Bilal Amarni <bilal.amarni@gmail.com>
2017-03-14 15:27:08 +0100

    upgrade to debian stretch
    * add required packages not installed by default (systemd udev)
    * disable predictable network interfaces name through net.ifnames=0
    * execute dpkg config on first boot instead of a cross-arch chroot
    * add google's dns server for debugging purposes (eg. systemd fails)

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 # pi64
 
-pi64 is an experimental 64-bit OS for the Raspberry Pi 3. It is based on Debian Jessie and backed by a 4.9 Linux kernel.
+pi64 is an experimental 64-bit OS for the Raspberry Pi 3. It is based on Debian Stretch and backed by a 4.9 Linux kernel.
 
 ## Installation
 
 The latest image is always available in the [releases](https://github.com/bamarni/pi64/releases) section.
 
 Once downloaded, you can follow the [official instructions](https://www.raspberrypi.org/documentation/installation/installing-images/README.md) for writing it to your SD card.
+
+During first boot the installation process will continue for a few minutes, then the Raspberry Pi will reboot and you'll be ready to go.
 
 ## Getting started
 
--- build.sh
+++ build.sh
@@ -60,7 +60,7 @@ root_length=$(echo "$parted_out" | grep -e '^ 2'| xargs echo -n | cut -d" " -f 4
 boot_dev=$(losetup --show -f -o ${boot_offset} --sizelimit ${boot_length} pi64.img)
 root_dev=$(losetup --show -f -o ${root_offset} --sizelimit ${root_length} pi64.img)
 
-mkdosfs -n boot -F 32 -v $boot_dev
+mkdosfs -n boot -F 32 $boot_dev
 mkfs.ext4 -O ^huge_file $root_dev
 
 
@@ -68,58 +68,61 @@ mkfs.ext4 -O ^huge_file $root_dev
 # build rootfs
 
 mkdir -p mnt
-mount -v $root_dev mnt -t ext4
-
-mkdir -p mnt/dev
-mount -o bind /dev mnt/dev
+mount $root_dev mnt -t ext4
 
 multistrap -a arm64 -d $PWD/mnt -f ../multistrap.conf
 
 cp /usr/bin/qemu-aarch64-static mnt/usr/bin/qemu-aarch64-static
 
-cat << EOF | chroot mnt
-export DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true
-export LC_ALL=C LANGUAGE=C LANG=C
+chroot mnt apt-get clean
 
-cat > /etc/fstab <<EOL
+rm -rf mnt/var/lib/apt/lists/*
+
+rm mnt/usr/bin/qemu-aarch64-static
+
+cat > mnt/etc/fstab <<EOL
 proc            /proc           proc    defaults          0       0
 /dev/mmcblk0p1  /boot           vfat    defaults          0       2
 /dev/mmcblk0p2  /               ext4    defaults,noatime  0       1
 EOL
 
-mount -t proc proc /proc
-mount -t sysfs sys /sys
+echo exit 101 > mnt/usr/sbin/policy-rc.d
 
-echo exit 101 > /usr/sbin/policy-rc.d
-chmod +x /usr/sbin/policy-rc.d
+chmod +x mnt/usr/sbin/policy-rc.d
 
-/var/lib/dpkg/info/dash.preinst install
-dpkg --configure -a
+echo raspberrypi > mnt/etc/hostname
 
-rm /usr/sbin/policy-rc.d
+echo 127.0.1.1 raspberrypi >> mnt/etc/hosts
 
-echo raspberrypi > /etc/hostname
+echo nameserver 8.8.8.8 > mnt/etc/resolv.conf
 
-echo 127.0.1.1 raspberrypi >> /etc/hosts
-
-cat >> /etc/network/interfaces <<EOL
+cat >> mnt/etc/network/interfaces <<EOL
 auto eth0
 iface eth0 inet dhcp
 EOL
 
-useradd -s /bin/bash --create-home -p $(perl -e 'print crypt("raspberry", "password")') pi
-echo "pi ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/010_pi-nopasswd
-
-cat > /root/init_setup.sh <<EOL
+cat > mnt/root/init_setup.sh <<EOL
 #!/bin/sh
+
+export DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true
+export LC_ALL=C LANGUAGE=C LANG=C
+export PATH=/usr/sbin:/usr/bin:/sbin:/bin
 
 mount -t proc proc /proc
 mount -t sysfs sys /sys
 mount /boot
 mount -o remount,rw /
 
-parted /dev/mmcblk0 u s resizepart 2 \\\$(expr \\\$(cat /sys/block/mmcblk0/size) - 1)
+parted /dev/mmcblk0 u s resizepart 2 \$(expr \$(cat /sys/block/mmcblk0/size) - 1)
 resize2fs /dev/mmcblk0p2
+
+/var/lib/dpkg/info/dash.preinst install
+dpkg --configure -a
+
+rm /usr/sbin/policy-rc.d
+
+useradd -s /bin/bash --create-home -p $(perl -e 'print crypt("raspberry", "password")') pi
+echo "pi ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/010_pi-nopasswd
 
 sed -i 's| init=/root/init_setup.sh||' /boot/cmdline.txt
 sync
@@ -129,20 +132,14 @@ rm /root/init_setup.sh
 echo b > /proc/sysrq-trigger
 EOL
 
-chmod +x /root/init_setup.sh
-
-apt-get clean
-rm -rf /var/lib/apt/lists/*
-EOF
-
-rm mnt/usr/bin/qemu-aarch64-static
+chmod +x mnt/root/init_setup.sh
 
 
 
 # install boot stuff
 
 mkdir -p mnt/boot
-mount -v $boot_dev mnt/boot -t vfat
+mount $boot_dev mnt/boot -t vfat
 
 cp -r ../boot/* mnt/boot
 
@@ -156,5 +153,5 @@ cd ..
 
 # compress image
 
-umount mnt/boot mnt/dev mnt/proc mnt/sys mnt
-zip pi64.img.zip pi64.img
+umount mnt/boot mnt
+zip pi64.zip pi64.img
